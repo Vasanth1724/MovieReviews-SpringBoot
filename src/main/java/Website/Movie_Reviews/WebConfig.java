29ed4c10921e8f25e1de1e768e3097440637faf6
Vasanth1724 <kvasanthkumar1724@gmail.com>
2025-02-04 23:15:12 +0530

    changes in webconfig

--- src/main/java/Website/Movie_Reviews/WebConfig.java
+++ src/main/java/Website/Movie_Reviews/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://movie-reviews-react.vercel.app") // Match your frontend URL
