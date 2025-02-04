83fde00f56492b3d6e26ba9bddafcc1e6f1176dc
Vasanth1724 <kvasanthkumar1724@gmail.com>
2025-02-04 19:35:01 +0530

    changes in url

--- src/main/java/Website/Movie_Reviews/WebConfig.java
+++ src/main/java/Website/Movie_Reviews/WebConfig.java
@@ -1,6 +1,5 @@
 package Website.Movie_Reviews;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("*") // Allow your frontend's URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow cookies/credentials if needed
+        registry.addMapping("/**")
+                .allowedOrigins("https://movie-reviews-react.vercel.app") // Match your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true); // If you're sending cookies or authentication headers
     }
 }
